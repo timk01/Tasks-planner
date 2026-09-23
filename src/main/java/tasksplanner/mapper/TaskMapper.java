8c9dd9a02d77261538d8019fff0ffac860ef6975
Timur <timk01@mail.ru>
2026-09-23 18:33:00 +0300

    перестроил показ времени юзеру при получении списка задач;

--- src/main/java/tasksplanner/mapper/TaskMapper.java
+++ src/main/java/tasksplanner/mapper/TaskMapper.java
@@ -3,10 +3,13 @@ package tasksplanner.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 import tasksplanner.entity.Task;
 import tasksplanner.response.ScheduledTaskResponse;
 import tasksplanner.response.TaskResponse;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Mapper(
@@ -14,10 +17,21 @@ import java.util.List;
 )
 public interface TaskMapper {
 
+    ZoneId MOSCOW_ZONE = ZoneId.of("Europe/Moscow");
+
     @Mapping(source = "id", target = "taskId")
     @Mapping(source = "taskStatus", target = "status")
     @Mapping(source = "taskOwner.id", target = "ownerId")
+    @Mapping(source = "finishedAt", target = "finishedAt", qualifiedByName = "toMoscowOffset")
     TaskResponse toTaskResponse(Task task);
+
+    @Named("toMoscowOffset")
+    default OffsetDateTime toMoscowOffset(OffsetDateTime time) {
+        return time == null
+                ? null
+                : time.atZoneSameInstant(ZoneId.of("Europe/Moscow"))
+                .toOffsetDateTime();
+    }
 
     List<TaskResponse> toTaskResponseList(List<Task> tasks);
 
